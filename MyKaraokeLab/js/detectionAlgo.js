1d18b65eb3c83b7378df3a0ce9d21c4db0541d8c
Nordine Bouchti <nordine.bouchti@etu.unice.fr>
2017-02-25 16:13:03 +0100

    Fonction pour retrouver la cent de la note

--- MyKaraokeLab/js/detectionAlgo.js
+++ MyKaraokeLab/js/detectionAlgo.js
@@ -6,3 +6,18 @@ $.getJSON('js/notes.json', function (data) {
 	notesTable = data;
 	notesArray = notesTable[baseFreq.toString()];
 });
+
+
+
+
+
+
+var findCentsOffPitch = function(freq, refFreq) {
+    // Nous devons trouver dans quelle mesure freq est de baseFreq en cents
+    var log2 = 0.6931471805599453; // Math.log(2)
+    var multiplicativeFactor = freq / refFreq;
+    
+    // Nous utilisons Math.floor pour obtenir la partie entière et ignorer les décimales
+    var cents = Math.floor(1200 * (Math.log(multiplicativeFactor) / log2));
+    return cents;
+};

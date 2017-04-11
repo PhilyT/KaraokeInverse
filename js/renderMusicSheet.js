dc6a5c65db99ef33ff0a9565a03111f908eb6129
Estelle Rostan <estelle.rostan@etu.unice.fr>
2017-04-11 11:01:15 +0200

    #6 Supprime la clef et la  mesure
    On ne les prends pas en considération pour l'instant.

--- js/renderMusicSheet.js
+++ js/renderMusicSheet.js
@@ -27,16 +27,10 @@ function renderScore() {
     context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
 // Create a stave of width 400 at position 10, 40 on the canvas.
-    stave = new VF.Stave(10, 40, 400);
-// Create a stave of width 400 at position 410, 40 on the canvas.
-    stave2 = new VF.Stave(410, 40, 400);
-
-// Add a clef and time signature.
-    stave.addClef("treble").addTimeSignature("4/4");
+    stave = new VF.Stave(10, 40, 1000);
 
 // Connect it to the rendering context and draw!
     stave.setContext(context).draw();
-    stave2.setContext(context).draw();
 }
 
 function render(note) {
@@ -44,10 +38,6 @@ function render(note) {
     renderScore();
     dummNotes.push(new Vex.Flow.StaveNote({clef: "treble", keys: [note], duration: "h"}));
 
-    var otherNotes = [];
-    otherNotes.push(new Vex.Flow.StaveNote({clef: "treble", keys: ["g/4"], duration: "q"}));
-
 // Helper function to justify and draw a 4/4 voice
     VF.Formatter.FormatAndDraw(context, stave, dummNotes);
-    VF.Formatter.FormatAndDraw(context, stave2, otherNotes);
 }

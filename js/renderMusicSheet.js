0a504a28edbe2e16cd1a286b817e5bdbd026505b
MAROUANE ML <marouane-93@outlook.com>
2017-05-28 20:28:41 +0200

    #21 moins de hauteur pour les partitions

--- js/renderMusicSheet.js
+++ js/renderMusicSheet.js
@@ -41,12 +41,12 @@ function renderScore() {
     renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
 // Configure the rendering context.
-    renderer.resize(1000, 500);
+    renderer.resize(1000, 230);
     context = renderer.getContext();
     context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
 // Create a stave of width 400 at position 10, 40 on the canvas.
-    stave = new VF.Stave(10, 40, 1000);
+    stave = new VF.Stave(10, 40, 230);
 // Connect it to the rendering context and draw!
     stave.setContext(context).draw();
 }

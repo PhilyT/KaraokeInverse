552f3256dff2495ac68138bb5fd72b81b7216681
Tom <tom.phily@live.fr>
2017-05-31 19:19:50 +0200

    #21 Retour à la ligne foctionnel

--- js/renderMusicSheet.js
+++ js/renderMusicSheet.js
@@ -4,7 +4,8 @@ var renderer;
 var context;
 var stave;
 var dummNotes;
-
+var cptnotedanspartition = 0;
+var nombredepartition = 1;
 var clearVexCavnasBtn;
 
 $(document).ready(function () {
@@ -16,9 +17,9 @@ $(document).ready(function () {
     
     renderScore();
     dummNotes = [];
-
+    dummNotes.push([]);
 // Helper function to justify and draw a 4/4 voice
-    VF.Formatter.FormatAndDraw(context, stave, dummNotes);
+    VF.Formatter.FormatAndDraw(context, stave[0], dummNotes[nombredepartition-1]);
 });
 
 function newPatition(stave){
@@ -29,34 +30,63 @@ function newPatition(stave){
 
 function clearVexCavnas() {
     dummNotes = [];
+    dummNotes.push([]);
+    cptnotedanspartition =0;
+    nombredepartition=1;
     div.innerHTML = "";
     renderScore();
 }
 
 function renderScore() {
+
     VF = Vex.Flow;
 
-// Create an SVG renderer and attach it to the DIV element named "vex-canvas".
+    // Create an SVG renderer and attach it to the DIV element named "vex-canvas".
     div = document.getElementById("vex-canvas");
+
     renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
-// Configure the rendering context.
+    // Configure the rendering context.
     renderer.resize(1000, 230);
     context = renderer.getContext();
     context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
-// Create a stave of width 400 at position 10, 40 on the canvas.
-    stave = new VF.Stave(10, 40, 1000);
-// Connect it to the rendering context and draw!
-    stave.setContext(context).draw();
+    // Create a stave of width 400 at position 10, 40 on the canvas.
+    stave = [];
+    for(var i=0;i<nombredepartition;i++){
+        if(i==0)
+        {
+            stave.push(new VF.Stave(10, 40, 1000));
+        }
+        else
+        {
+            stave.push(new VF.Stave(10, ((i+1)*120), 1000));
+        }
+        // Connect it to the rendering context and draw!
+        stave[i].setContext(context).draw();
+    }
+
+
 }
 
 function render(note) {
+    cptnotedanspartition++;
     div.innerHTML = '';
+
+    if(cptnotedanspartition%12 == 0){
+        nombredepartition++;
+        dummNotes.push([]);
+        dummNotes[nombredepartition-1].push(new Vex.Flow.StaveNote({clef: "treble", keys: [note.note], duration: note.duration}));
+    }else{
+        dummNotes[nombredepartition-1].push(new Vex.Flow.StaveNote({clef: "treble", keys: [note.note], duration: note.duration}));
+    }
+
     renderScore();
 
-    dummNotes.push(new Vex.Flow.StaveNote({clef: "treble", keys: [note.note], duration: note.duration}));
+    for(var i=0; i<nombredepartition; i++){
+        // Helper function to justify and draw a 4/4 voice
+        VF.Formatter.FormatAndDraw(context, stave[i], dummNotes[i]);
+    }
 
-// Helper function to justify and draw a 4/4 voice
-    VF.Formatter.FormatAndDraw(context, stave, dummNotes);
+
 }
